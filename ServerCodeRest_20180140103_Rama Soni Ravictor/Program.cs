abd793972c582a3e8db06f601962e611cab61ba8
Rama Soni Ravictor <55401842+ravictor21@users.noreply.github.com>
2020-12-21 13:39:55 +0700

    Add project files.

---
+++ ServerCodeRest_20180140103_Rama Soni Ravictor/Program.cs
@@ -0,0 +1,49 @@
+﻿using ServiceRest_20180140103_Rama_Soni_Ravictor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCodeRest_20180140103_Rama_Soni_Ravictor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+            Uri address = new Uri("http://localhost:1907/Mahasiswa");
+            WebHttpBinding binding = new WebHttpBinding();
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY), address);
+                hostObjek.AddServiceEndpoint(typeof(ITI_UMY), binding, "");
+
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                hostObjek.Description.Behaviors.Add(smb);
+                Binding mexbinding = MetadataExchangeBindings.CreateMexHttpBinding();
+                hostObjek.AddServiceEndpoint(typeof(IMetadataExchange), mexbinding, "mex");
+
+                WebHttpBehavior whb = new WebHttpBehavior();
+                whb.HelpEnabled = true;
+                hostObjek.Description.Endpoints[0].EndpointBehaviors.Add(whb);
+
+                hostObjek.Open();
+                Console.WriteLine("Server ready . .");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
